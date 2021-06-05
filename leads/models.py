e3ed5f637f337fcd61db221c683a034a71a9fa67
Imnmania <niloyb@live.com>
2021-06-06 03:48:53 +0600

    create user/ admin panel access

--- leads/models.py
+++ leads/models.py
@@ -12,6 +12,9 @@ class Lead(models.Model):
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
     # agent = models.ForeignKey("Agent", on_delete=models.SET_NULL, null=True)
     # agent = models.ForeignKey("Agent", on_delete=models.SET_DEFAULT, default=)
     
@@ -35,4 +38,7 @@ class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # first_name = models.CharField(max_length=20)
     # last_name = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.user.email
     
