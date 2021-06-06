802bbc68f88524df59fd59927bb1d6a00b20681a
Imnmania <niloyb@live.com>
2021-06-06 15:57:45 +0600

    passing context

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead
+
 
 def home_page(request):
     # return HttpResponse('Hello Warudo!')
-    return render(request, "second_page.html")
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+        # "name": "Joe",
+        # "age": 26
+    }
+    return render(request, "second_page.html", context)
